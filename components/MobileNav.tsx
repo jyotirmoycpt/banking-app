dddcdf114bc85997796d4875df6e5f5255f8866d
jyotirmoy99 <bjyotirmoy636@gmail.com>
2025-06-08 14:10:39 +0530

    feat: implement authentication forms and UI components for sign-in and sign-up

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -39,10 +39,10 @@ const MobileNav = ({ user }: MobileNavProps) => {
               src="/icons/logo.svg"
               width={34}
               height={34}
-              alt="Horizon logo"
+              alt="Fundiqo logo"
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
-              Horizon
+              Fundiqo
             </h1>
           </Link>
           <div className="mobilenav-sheet">
